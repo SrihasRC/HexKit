a163e4969fd7622031bad55262106c538de7d51f
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-05-22 20:17:41 +0530

    fix(theme): remove invalid type import from next-themes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 import { useEffect } from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: any) {
   // Force dark mode and disable theme switching
   useEffect(() => {
     document.documentElement.classList.add('dark');
